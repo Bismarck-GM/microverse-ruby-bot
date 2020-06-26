199490cb34591141dc05f8bbeb1830eba150c422
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-26 20:04:25 -0300

    Add basic css for cards pages

--- lib/card_links.rb
+++ lib/card_links.rb
@@ -5,4 +5,4 @@ hypnotoad = 'https://raw.githack.com/Bismarck-GM/microverse-ruby-bot/base-bot-te
 help = 'https://raw.githack.com/Bismarck-GM/microverse-ruby-bot/base-bot-template/responses_links/help.html'
 question = 'https://raw.githack.com/Bismarck-GM/microverse-ruby-bot/base-bot-template/responses_links/question.html'
 no_match = 'https://raw.githack.com/Bismarck-GM/microverse-ruby-bot/base-bot-template/responses_links/nomatch.html'
-trivia_link = ''
+trivia_link = 'https://raw.githack.com/Bismarck-GM/microverse-ruby-bot/base-bot-template/responses_links/trivia.html'
