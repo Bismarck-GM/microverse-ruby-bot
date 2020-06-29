c420687ab07cad43ea3679ab0e8461533dafc1b8
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-29 00:20:56 -0300

    Fix rubocop offenses

--- spec/reply_handler_spec.rb
+++ spec/reply_handler_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require '../lib/reply_handler.rb'
 
@@ -12,25 +14,25 @@ class Tweet
 end
 
 describe '#empty_help' do
-  let(:tweet) { Tweet.new('help', 00000000001) }
-  let(:tweet2) { Tweet.new('hello dude', 00000000001) }
+  let(:tweet) { Tweet.new('help', 0o0000000001) }
+  let(:tweet2) { Tweet.new('hello dude', 0o0000000001) }
   let(:arr) { [1, 2, 3] }
-  it 'Should return True if the txt field of the Tweet object contain string for help or only contains the name of the Bot' do
+  it 'Should return True if the txt field of the Tweet object contain string' do
     expect(empty_help(tweet)).to eql(true)
   end
-  
-  it 'Should return False if the txt field of the Tweet object does not contain string for help or the name of the Bot' do
+
+  it 'Should return False if the txt field of the Tweet object does not contain string' do
     expect(empty_help(tweet2)).to eql(false)
   end
-  
+
   it 'Should raise ScriptError if argument is not a Tweet Object' do
     expect { empty_help(arr) }.to raise_error(ScriptError)
   end
 end
 
 describe '#question' do
-  let(:tweet) { Tweet.new('Are you sure about it?', 00000000001) }
-  let(:tweet2) { Tweet.new('hello dude', 00000000001) }
+  let(:tweet) { Tweet.new('Are you sure about it?', 0o0000000001) }
+  let(:tweet2) { Tweet.new('hello dude', 0o0000000001) }
   let(:arr) { [1, 2, 3] }
   it "Should return True if the txt field of the Tweet object contain string for '?' in any position" do
     expect(question(tweet)).to eql(true)
@@ -39,38 +41,38 @@ describe '#question' do
   it "Should return False if the txt field of the Tweet object does not contain string '?'" do
     expect(question(tweet2)).to eql(false)
   end
-  
+
   it 'Should raise ScriptError if argument is not a Tweet Object' do
     expect { question(arr) }.to raise_error(ScriptError)
   end
 end
 
 describe '#idolize' do
-  let(:tweet) { Tweet.new('heaven', 00000000001) }
-  let(:tweet2) { Tweet.new('hello dude', 00000000001) }
+  let(:tweet) { Tweet.new('heaven', 0o0000000001) }
+  let(:tweet2) { Tweet.new('hello dude', 0o0000000001) }
   let(:arr) { [1, 2, 3] }
-  it "Should return True if the txt field of the Tweet object contains any of the Strings List" do
+  it 'Should return True if the txt field of the Tweet object contains any of the Strings List' do
     expect(idolize(tweet)).to eql(true)
   end
 
-  it "Should return False if the txt field of the Tweet object does not contains any of the Strings List" do
+  it 'Should return False if the txt field of the Tweet object does not contains any of the Strings List' do
     expect(idolize(tweet2)).to eql(false)
   end
-  
+
   it 'Should raise ScriptError if argument is not a Tweet Object' do
     expect { idolize(arr) }.to raise_error(ScriptError)
   end
 end
 
 describe '#tohell' do
-  let(:tweet) { Tweet.new('hell', 00000000001) }
-  let(:tweet2) { Tweet.new('hello dude', 00000000001) }
+  let(:tweet) { Tweet.new('hell', 0o0000000001) }
+  let(:tweet2) { Tweet.new('hello dude', 0o0000000001) }
   let(:arr) { [1, 2, 3] }
-  it "Should return True if the txt field of the Tweet object contains any of the Strings List" do
+  it 'Should return True if the txt field of the Tweet object contains any of the Strings List' do
     expect(tohell(tweet)).to eql(true)
   end
 
-  it "Should return False if the txt field of the Tweet object does not contains any of the Strings List" do
+  it 'Should return False if the txt field of the Tweet object does not contains any of the Strings List' do
     expect(tohell(tweet2)).to eql(false)
   end
 
@@ -80,14 +82,14 @@ describe '#tohell' do
 end
 
 describe '#hypnotoad' do
-  let(:tweet) { Tweet.new('hypnosis', 00000000001) }
-  let(:tweet2) { Tweet.new('hello dude', 00000000001) }
+  let(:tweet) { Tweet.new('hypnosis', 0o0000000001) }
+  let(:tweet2) { Tweet.new('hello dude', 0o0000000001) }
   let(:arr) { [1, 2, 3] }
-  it "Should return True if the txt field of the Tweet object contains any of the Strings List" do
+  it 'Should return True if the txt field of the Tweet object contains any of the Strings List' do
     expect(hypnotoad(tweet)).to eql(true)
   end
 
-  it "Should return False if the txt field of the Tweet object does not contains any of the Strings List" do
+  it 'Should return False if the txt field of the Tweet object does not contains any of the Strings List' do
     expect(hypnotoad(tweet2)).to eql(false)
   end
 
